44bd0442e948176f55dc316beaa9fe73387cb55b
xuetong <xuetong@umich.edu>
2019-03-24 15:33:41 -0400

    added gan to models

--- models.py
+++ models.py
@@ -74,3 +74,128 @@ class G1_Net(nn.Module):
         x=self.final(x4)
         return x
 
+class G2_Net(nn.Module):
+
+    def __init__(self, kernel_size=3):
+        super(G2_Net, self).__init__()
+        #input size (6,256,256)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(6,32,kernel_size,stride = 1, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(32,32,kernel_size,stride = 1, padding=1),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(3,stride=2,padding=1))
+        #(3,128,128)
+        
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32,64,kernel_size,stride = 1, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(64,64,kernel_size,stride = 1, padding=1),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(3,stride=2,padding=1))
+        #(64,64,64)
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64,128,kernel_size,stride = 1, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(128,128,kernel_size,stride = 1, padding=1),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(3,stride=2,padding=1))
+        #(128,32,32)
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(128,256,kernel_size,stride = 1, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(256,256,kernel_size,stride = 1, padding=1),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(3,stride=2,padding=1))
+        #(256,16,16)
+        self.deconv4 = nn.Sequential(
+            nn.ConvTranspose2d(256,128,kernel_size,stride = 1, padding=2),
+            nn.LeakyReLU(),
+            nn.Conv2d(128,128,kernel_size,stride = 1, padding=2),
+            nn.LeakyReLU(),
+            nn.Upsample(scale_factor=2, mode='bilinear'))
+        #(128,32,32)
+        self.deconv3 = nn.Sequential(
+            nn.ConvTranspose2d(256,64,kernel_size,stride = 1, padding=2),
+            nn.LeakyReLU(),
+            nn.Conv2d(64,64,kernel_size,stride = 1, padding=2),
+            nn.LeakyReLU(),
+            nn.Upsample(scale_factor=2, mode='bilinear'))
+        #(64,64,64)
+        self.deconv2 = nn.Sequential(
+            nn.ConvTranspose2d(128,32,kernel_size,stride = 1, padding=2),
+            nn.LeakyReLU(),
+            nn.Conv2d(32,32,kernel_size,stride = 1, padding=2),
+            nn.LeakyReLU(),
+            nn.Upsample(scale_factor=2, mode='bilinear'))
+        #(3,256,256)
+        self.deconv1 = nn.Sequential(
+            nn.ConvTranspose2d(64,3,kernel_size,stride = 1, padding=2),
+            nn.LeakyReLU(),
+            nn.Conv2d(3,3,kernel_size,stride = 1, padding=2),
+            nn.LeakyReLU(),
+            nn.Upsample(scale_factor=2, mode='bilinear'))
+        
+    def forward(self,inputs):
+        e1 = self.conv1(inputs)
+        e2 = self.conv2(e1)
+        e3 = self.conv3(e2)
+        e4 = self.conv4(e3)
+        d4 = self.deconv4(e4)
+        d3_in = torch.cat((e3,d4),1)
+        d3 = self.deconv3(d3_in)
+        d2_in = torch.cat((e2,d3),1)
+        d2 = self.deconv2(d2_in)
+        d1_in = torch.cat((e1,d2),1)
+        d1 = self.deconv1(d1_in)
+        return d1
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self, kernel_size=5, dim=64):
+        super(Discriminator, self).__init__()
+        self.kernel_size = kernel_size
+        self.dim = dim
+        self.conv1 = nn.Conv2d(3, self.dim, self.kernel_size, stride=2, padding=2)
+        #(64,128,128)
+        #LeakyRelu
+        self.conv2 = nn.Conv2d(self.dim, 2*self.dim,kernel_size,stride=2,padding=2)
+        self.bn1 = nn.BatchNorm2d(2*self.dim)
+        #LeakyRelu
+        #(128,64,64)
+        self.conv3 = nn.Conv2d(2*self.dim,4*self.dim,kernel_size,stride=2,padding=2)
+        self.bn2 = nn.BatchNorm2d(4*self.dim)
+        #LeakyRelu
+        #(256,32,32)
+        self.conv4 = nn.Conv2d(4*self.dim,8*self.dim,kernel_size,stride=2,padding=2)
+        self.bn3 = nn.BatchNorm2d(8*self.dim)
+        #LeakyRelu
+        #(512,16,16)
+        self.conv5 = nn.Conv2d(8*self.dim,8*self.dim,kernel_size,stride=2,padding=2)
+        self.bn4 = nn.BatchNorm2d(8*self.dim)
+        #LeakyRelu
+        #(512,8,8)
+        self.fc = nn.Linear(8*10*8*self.dim,1)
+        
+    def forward(self, x):
+        batch_size, channel, height, width = x.shape
+        output = self.conv1(x)
+        output = F.leaky_relu(output)
+        output = self.conv2(output)
+        output = self.bn1(output)
+        output = F.leaky_relu(output)
+        output = self.conv3(output)
+        output = self.bn2(output)
+        output = F.leaky_relu(output)
+        output = self.conv4(output)
+        output = self.bn3(output)
+        output = F.leaky_relu(output)
+        output = self.conv5(output)
+        output = self.bn4(output)
+        output = F.leaky_relu(output)
+        
+        output = output.view(-1, 8*10*8*self.dim)
+        output = self.fc(output)
+        
+        return output
